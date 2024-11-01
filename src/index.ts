f6d102cf1a26c88291931748451111c17e692f89
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-11-01 23:25:37 +0530

    SETUP: Setup the env, tsconfig, nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+import express from "express";
+
+const app = express();
+
+app.get("/", function (req: express.Request, res: express.Response) {
+  res.status(400).send("Hello World!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started on port 3000");
+});
