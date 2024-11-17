cc90572e42336aeb9ba909c8f877f1c158d9c163
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-11-17 11:35:33 +0530

    ADD: Added new migration

---
+++ prisma/migrations/20241117060509_init/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [spotify] on the enum `LayoutItemType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "LayoutItemType_new" AS ENUM ('text', 'image', 'link', 'section', 'empty', 'html', 'carousel');
+ALTER TABLE "LayoutItem" ALTER COLUMN "type" TYPE "LayoutItemType_new" USING ("type"::text::"LayoutItemType_new");
+ALTER TYPE "LayoutItemType" RENAME TO "LayoutItemType_old";
+ALTER TYPE "LayoutItemType_new" RENAME TO "LayoutItemType";
+DROP TYPE "LayoutItemType_old";
+COMMIT;
