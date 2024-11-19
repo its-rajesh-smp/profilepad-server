a5d4a19596c3b0d4269c62961454250630205584
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-11-19 07:38:20 +0530

    REFACTOR: Refactored the code for production build

--- src/api/utils/jwt.util.ts
+++ src/api/utils/jwt.util.ts
@@ -1,8 +1,12 @@
 import jwt from "jsonwebtoken";
 import { getEnv } from "./env.util";
 
-export const createJWTToken = (payload: any) =>
-  jwt.sign(payload, getEnv("JWT_SECRET"));
+export const createJWTToken = (payload: any) => {
+  const JWT_SECRET = getEnv("JWT_SECRET") || "secret";
+  return jwt.sign(payload, JWT_SECRET);
+};
 
-export const verifyJWTToken = (token: string) =>
-  jwt.verify(token, getEnv("JWT_SECRET"));
+export const verifyJWTToken = (token: string) => {
+  const JWT_SECRET = getEnv("JWT_SECRET") || "secret";
+  return jwt.verify(token, JWT_SECRET);
+};
