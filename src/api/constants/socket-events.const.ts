b69289f9bac673156cfb192c7bb007c1173c1162
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-11-28 08:04:11 +0530

    ADD: Added socket connection

---
+++ src/api/constants/socket-events.const.ts
@@ -0,0 +1,5 @@
+export const SOCKET_EVENTS = {
+  CONNECTION: "connection",
+  DISCONNECT: "disconnect",
+  PLAYER_JOINED: "playerJoined",
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import cors from "cors";
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { SOCKET } from "./api/constants/app.const";
 import router from "./api/routes/index.routes";
+import SocketService from "./api/services/Socket.service";
 
 export const app = express();
+const httpServer = createServer(app);
+
+/* Initialize Socket.IO */
+const io =
+  SOCKET &&
+  new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
 
 /* Middlewares */
 app.use(express.json());
@@ -11,7 +26,12 @@ app.use(cors({ origin: "*" }));
 /* Routes */
 app.use(router);
 
+/* Creating a new instance of SocketService and initializing it */
+if (io) {
+  new SocketService(io).initialize();
+}
+
 /* Start Server */
-app.listen(process.env.PORT || 3000, () => {
+httpServer.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000");
 });
