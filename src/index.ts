590f7b8ed8cc37050a3a65cbae1a972428d7778e
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-12-12 18:36:49 +0530

    REMOVE: Removed the game and socket elements

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
 import cors from "cors";
 import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
 import router from "./api/routes/index.routes";
-import SocketService from "./api/services/Socket.service";
 
 export const app = express();
-const httpServer = createServer(app);
-
-/* Initialize Socket.IO */
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
 
 /* Middlewares */
 app.use(express.json());
@@ -23,12 +11,9 @@ app.use(cors({ origin: "*" }));
 /* Routes */
 app.use(router);
 
-/* Creating a new instance of SocketService and initializing it */
-new SocketService(io).initialize();
-
 /* Start Server */
 const port = parseInt(process.env.PORT || "3000");
 
-httpServer.listen(port, "0.0.0.0", () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server started on port ${port}`);
 });
